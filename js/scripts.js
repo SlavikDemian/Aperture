757d2cfc7d888ee4862412a7285487f62637d135
SlavikDemian <slavikdemian@gmail.com>
2023-04-20 16:01:22 +0300

    JS carousel 1

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,18 @@
 $(document).ready(function(){
     $('.owl-carousel').owlCarousel({
         loop:true,
-        margin:0,
+        margin:20,
         nav:true,
+        autoplay: true,
+        autoplayTimeout: 2000,
         navText: [ '', '' ],
 
         responsive:{
             0:{
                 items:1
+            },
+            800:{
+                items:3
             },
 
             1000:{
@@ -15,4 +20,8 @@ $(document).ready(function(){
             }
         }
     });
+    $( function() {
+        $( "#accordion" ).accordion();
+    } );
 });
+
